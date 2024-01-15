76bcc51da3791bc581b44d06a6d7f038c8f13722
Eric Youn <ericyoun430@gmail.com>
2024-01-15 14:45:41 -0800

    html skeleton

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import CheckBox from './todo-items/header/checkbox.png';
+import './styles.css';
+
+//Add New Project
+document.querySelector('.project-add').addEventListener('click',()=> {
+
+})
+
+//Add New To-Do Item
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
 const path = require('path');
+const HTMLWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname,'dist'),
-    }
+        clean: true,
+    },
+    devServer: {
+        static: './src',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader'],
+            },
+            {
+                test: /\.(jpg|png|svg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.ttf$/i,
+                type: "asset/resource",
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebPackPlugin({
+            template: './src/template.html',
+        }),
+    ],
 }
